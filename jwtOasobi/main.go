109a5a37e9a87dfff3ab9112cde31fb85617ab6d
Mackyson <mackyson72@gmail.com>
2020-11-16 00:23:03 +0900

    JWTおためし

---
+++ jwtOasobi/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+const secret = "gomi"
+
+func main() {
+	fmt.Println("----generate phase----")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		IssuedAt: time.Now().Unix(),
+		Subject:  "1",
+	})
+	s, err := token.SignedString([]byte(secret))
+	fmt.Printf("%s\n%v\n", s, err)
+	fmt.Printf("%s\n", token.Method.Alg())
+
+	fmt.Println("----parse phase----")
+
+	token_parse, err := jwt.Parse(s, func(token *jwt.Token) (interface{}, error) {
+		if token.Method.Alg() == "HS256" {
+			return getKey(), nil
+		} else {
+			return nil, fmt.Errorf("Unexpected signing method %s", token.Method.Alg())
+		}
+	})
+	if err == nil {
+		fmt.Printf("Good!\n%v", token_parse.Claims)
+	} else {
+		if err == jwt.ErrInvalidKey {
+			fmt.Println("Too Bad!\nInvalid signature")
+		} else {
+			fmt.Println("OMG!\n%s", err)
+		}
+	}
+}
+func getKey() []byte {
+	return []byte(secret + "boke")
+}
